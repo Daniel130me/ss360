2d1459c653da41b227a2a63266cef1f9f3d84af6
Daniel130me <kosokodaniel@gmail.com>
2026-03-22 01:21:19 +0100

    add: setup for automatic deployment

--- index.php
+++ index.php
@@ -2,6 +2,24 @@
 date_default_timezone_set('Africa/Lagos');
 session_start();
 error_reporting(E_ALL);
+
+// Build the current URL dynamically
+$currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
+$currentUrl .= "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+// Parse the URL
+$parsedUrl = parse_url($currentUrl);
+$host = $parsedUrl['host'];
+if ($host == "localhost") {
+    $par = 1;
+    $_SESSION['env'] = 'dev';
+} else {
+    $par = 0;
+    $_SESSION['env'] = 'prod';
+}
+
+
+
 $url = $_SERVER["REQUEST_URI"];
 $parameter = explode("/", $url);
 
@@ -14,14 +32,15 @@ include_once("model/connect.php");
 // function check() {
 
 // }
+// $_SESSION['']
+$school_short = $parameter[1 + $par];
 
-$school_short = $parameter[1];
-if($school_short != $_SESSION['url'] or !isset($_SESSION['url'])){
-    $check = mysqli_query($conn,"SELECT url,session_id, phone1 FROM school WHERE url='$school_short'");
+if ($school_short != $_SESSION['url'] or !isset($_SESSION['url'])) {
+    $check = mysqli_query($conn, "SELECT url,session_id, phone1 FROM school WHERE url='$school_short'");
     // echo "takn page";
     // exit;
     // session_destroy();
-    if(mysqli_num_rows($check) < 1){
+    if (mysqli_num_rows($check) < 1) {
         include('404.php');
         // header("Location: 404");
         exit;
@@ -31,13 +50,13 @@ if($school_short != $_SESSION['url'] or !isset($_SESSION['url'])){
 $_SESSION['url'] = $school_short;
 // exit;z
 if (isset($_GET['id'])) {
-    $new_url = explode("?", $parameter[2]);
-     if ($new_url[0] == 'view_student_result') {
+    $new_url = explode("?", $parameter[2 + $par]);
+    if ($new_url[0] == 'view_student_result') {
         include('view_student_result.php');
         exit;
     }
-    $new_url = explode("?", $parameter[2]);
-     if ($new_url[0] == 'register_staff_self') {
+    $new_url = explode("?", $parameter[2 + $par]);
+    if ($new_url[0] == 'register_staff_self') {
         include('register_staff_self.php');
         exit;
     }
@@ -79,7 +98,7 @@ if (isset($_GET['id'])) {
         include('my_payment.php');
         exit;
     }
-    $new_url = explode("?", $parameter[2]);
+    $new_url = explode("?", $parameter[2 + $par]);
     if ($new_url[0] == 'report') {
         include('report_student.php');
         exit;
@@ -87,17 +106,17 @@ if (isset($_GET['id'])) {
 }
 // echo $parameter[2];
 // exit;
-if($parameter[2] == 'onboard') {
-    include("onboard/$parameter[3].php");
+if ($parameter[2 + $par] == 'onboard') {
+    include("onboard/" . $parameter[3 + $par] . ".php");
     exit;
 }
 // $pages = array("staff_attendance","assignments","results","analytics","assessment_result","take_assessment","student_portal","student_portal_profile","create_assessment","assessment","analytics","scan_qr","my_payment", "payment_success", "time_table", "lesson_note", "attendance","register_staff_self", "onboard", "404", "comment", "communication", "parent_portal", "waiting", "attendance", "registermyself", "dashboard", "change_password", "change_password_parent", "profile", "parent_profile", 'class', "staff", "employee", "each_user", "reports", "post_scores", "view_scores", "settings", "login", "students", "subjects", "logout");
-$pages = array("verify_otp","reset_password","enter_otp","forgot_password","graduates","comment_ai","student_class_notes", "payments", "assignments", "results", "analytics", "assessment_result", "take_assessment", "student_portal", "student_portal_profile", "create_assessment", "assessment", "scan_qr", "scan_staff_qr", "my_payment", "payment_success", "time_table", "lesson_note", "attendance", "staff_attendance", "onboard", "404", "comment", "communication", "parent_portal", "waiting", "attendance", "register_staff_self", "dashboard", "change_password", "change_password_parent", "profile", "parent_profile", 'class', "staff", "employee", "each_user", "reports", "post_scores", "view_scores", "settings", "login", "students", "subjects", "logout");
-if (in_array($parameter[2], $pages)) {
-    include($parameter[2] . ".php");
+$pages = array("verify_otp", "reset_password", "enter_otp", "forgot_password", "graduates", "comment_ai", "student_class_notes", "payments", "assignments", "results", "analytics", "assessment_result", "take_assessment", "student_portal", "student_portal_profile", "create_assessment", "assessment", "scan_qr", "scan_staff_qr", "my_payment", "payment_success", "time_table", "lesson_note", "attendance", "staff_attendance", "onboard", "404", "comment", "communication", "parent_portal", "waiting", "attendance", "register_staff_self", "dashboard", "change_password", "change_password_parent", "profile", "parent_profile", 'class', "staff", "employee", "each_user", "reports", "post_scores", "view_scores", "settings", "login", "students", "subjects", "logout");
+if (in_array($parameter[2 + $par], $pages)) {
+    include($parameter[2 + $par] . ".php");
     exit;
 }
-if ($parameter[2] == "") {
+if ($parameter[2 + $par] == "") {
     include("login.php");
     exit;
 }
--- model/connect.php
+++ model/connect.php
@@ -1,8 +1,23 @@
 <?php
-$conn = mysqli_connect('localhost','ekmapxmy_oluwagbenga','G.s.o.m.','ekmapxmy_ss360');
-mysqli_query($conn, "SET time_zone = 'Africa/Lagos'");
-// die('lk');
-if(!$conn) {
-    die("Error connecting to the database". mysqli_error($conn));
+$host =  $_SESSION['env'] == 'prod' ? 'ss360-db-do-user-34699889-0.d.db.ondigitalocean.com' : 'localhost';
+$user = $_SESSION['env'] == 'prod' ? 'doadmin' : 'root';
+$pass = $_SESSION['env'] == 'prod' ? 'AVNS_VGV1tkSdGGmh4pSBvX1' : '';
+$db = $_SESSION['env'] == 'prod' ? 'defaultdb' : 'ss360';
+$port = $_SESSION['env'] == 'prod' ? 25060 : 3306;
+$apply_ssl = $_SESSION['env'] == 'prod' ? MYSQLI_CLIENT_SSL : null;
+
+$conn = mysqli_init();
+if (!$conn) {
+    die('mysqli_init failed');
 }
-?>
+
+// Connect over SSL as required by DigitalOcean
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, $apply_ssl)) {
+    die("Connect Error: " . mysqli_connect_error());
+}
+
+mysqli_query($conn, "SET time_zone = 'Africa/Lagos'");
+
+if (!$conn) {
+    die("Error connecting to the database" . mysqli_error($conn));
+}
